672ebf123b68133b33a07cca4981ddb2d0199d38
Sobin Bikram Thapa <94164528+codesob@users.noreply.github.com>
2023-10-25 23:58:30 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import AboutUs from './components/AboutUs';
+import Services from './components/Services';
+import Support from './components/Support';
+import News from './components/News';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Client from './components/Client';
+import Contact from './components/Contact';
+import Body from './components/Body';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/support" element={<Support />} />
+          <Route path="/news" element={<News />} /> 
+          <Route path="/" exact component={Body} />
+          <Route path="/contact" element={<Contact />} />
+         
+       
+        </Routes>
+        <Client />
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
